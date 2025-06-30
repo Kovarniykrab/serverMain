34fae86c704a903af31310c1daf49aa0c34e6b7a
fora8284 <fora8284@gmail.com>
2025-06-30 11:34:20 +0500

    fix

--- config/config.go
+++ config/config.go
@@ -14,7 +14,6 @@ type JWT struct {
 	KeyLength   int    `long:"jwt-key-length" env:"SERVER_JWT_KEY_LENGTH"`
 	HourExpired int    `long:"jwt-hour-expired" env:"SERVER_JWT_HOUR_EXPIRED"`
 }
-
 type Web struct {
 	Host         string     `long:"web-host" env:"SERVER_WEB_HOST" default:"localhost"`
 	Port         int        `long:"web-port" env:"SERVER_WEB_PORT" default:"8080"`
@@ -28,14 +27,7 @@ type Web struct {
 	SameSiteNone int        `long:"web-same-site-none" env:"SERVER_WEB_SAME_SITE_NONE"`
 	LogLevel     slog.Level `long:"web-log-level" env:"SERVER_WEB_LOG_LEVEL"`
 }
-
 type PSQL struct {
-	Host           string `long:"psql-host" env:"SERVER_PSQL_HOST"`
-	Port           int    `long:"psql-port" env:"SERVER_PSQL_PORT" `
-	UserName       string `long:"psql-user-name" env:"SERVER_PSQL_USER_NAME" `
-	DBName         string `long:"psql-db-name" env:"SERVER_PSQL_DB_NAME" `
-	Password       string `long:"psql-password" env:"SERVER_PSQL_PASSWORD" `
-	SslMode        string `long:"psql-ssl-mode" env:"SERVER_PSQL_SSL_MODE" `
 	DSN            string `long:"psql-dsn" env:"SERVER_PSQL_DSN" `
 	Binary         bool   `long:"psql-binary" env:"SERVER_PSQL_BINARY"`
 	MaxConnections int    `long:"psql-max-connections" env:"SERVER_PSQL_MAX_CONNECTIONS"`
